38659c050847d486fa098c2037ae83866ccdd755
DEV1lmig <maquiroz@urbe.edu.ve>
2024-07-30 15:24:49 -0400

    little sematic fix

--- app/login/page.js
+++ app/login/page.js
@@ -103,7 +103,7 @@ export default function Login() {
               <input 
                 type="password" 
                 name="password"
-                placeholder="Password" 
+                placeholder="Contrase;a" 
                 className="w-full text-black px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
                 value={formData.password}
                 onChange={handleChange}
