c97a4f92f0e109df2146081e861b7ff55ac8e8f0
DEV1lmig <maquiroz@urbe.edu.ve>
2024-07-30 20:41:08 -0400

    eliminate image from forms

--- app/login/page.js
+++ app/login/page.js
@@ -79,21 +79,9 @@ export default function Login() {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
-      <div className="min-h-screen flex items-center justify-center bg-transparent p-6">
+      <div className="min-h-screen flex items-center justify-center bg-gray-800 p-6">
       <ToastContainer />
-        <div className="hidden md:block md:w-1/2 h-full">
-          <div className="relative w-full h-full">
-            <Image
-              src="/images/backgroundLogin.png" // Ensure you have a relevant image for the login page.
-              alt="CartCraft Login"
-              className="w-full h-full object-cover"
-              fill={true}
-            />
-          </div>
-        </div>
-        
-        {/* Form Section */}
-        <div className="w-full md:w-1/2 p-8 lg:p-12 flex flex-col items-center justify-center bg-white rounded-lg shadow-lg max-w-md">
+        <div className="w-full p-8 lg:p-12 flex flex-col items-center justify-center bg-white rounded-lg shadow-lg max-w-md">
           <h1 className="text-4xl font-bold text-blue-700 mb-4">
             Iniciar sesion en <span className="text-green-500">CartCraft</span>
           </h1>
--- app/register/page.js
+++ app/register/page.js
@@ -80,19 +80,9 @@ export default function Register() {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
-      <div className="min-h-screen flex items-center justify-center bg-transparent p-6">
+      <div className="min-h-screen flex items-center justify-center bg-gray-800 p-6">
         <ToastContainer />
-        <div className="hidden md:block md:w-1/2 h-full">
-          <div className="relative w-full h-full">
-            <Image
-              src="/images/backgroundRegister.png"
-              alt="Bienvenido a CartCraft"
-              className="w-full h-full object-cover"
-              fill={true}
-            />
-          </div>
-        </div>
-        <div className="w-full md:w-1/2 p-8 lg:p-12 flex flex-col items-center justify-center bg-white rounded-lg shadow-lg max-w-md">
+        <div className="w-full p-8 lg:p-12 flex flex-col items-center justify-center bg-white rounded-lg shadow-lg max-w-md">
           <h1 className="text-4xl font-bold text-blue-700 mb-4">
             Registrate en <span className="text-green-500">CartCraft</span>
           </h1>
